65aaad2b6b246a02cee1ad70ee924594111657b2
Rachita01 <rachitasinghal6@gmail.com>
2021-12-20 13:00:18 +0530

    Photo Gallery with search functionality

---
+++ src/data.js
@@ -0,0 +1,75 @@
+import img1 from './img/img1.webp';
+import img2 from './img/img2.webp';
+import img3 from './img/img3.webp';
+import img4 from './img/img4.webp';
+import img5 from './img/img5.webp';
+import img6 from './img/img6.webp';
+const data = {
+    galleryData:[
+    {
+      id:'1',
+      name:'img1',
+      
+      title:'blog 1',
+      desc:'desc 1',
+      img:img1,
+      
+    },
+    {
+        id:'2',
+        name:'img2',
+        title:'blog 2',
+        desc:'desc 2',
+        img:img2,
+
+    },
+    {
+      id:'3',
+      name:'img3',
+      title:'blog 3',
+      desc:'desc 3',
+      img:img3,
+    },
+    {
+        id:'4',
+        name:'img4',
+        
+        title:'blog 4',
+        desc:'desc 4',
+        img:img4,
+    },
+    {
+      id:'5',
+      name:'img5',
+      title:'blog 5',
+      desc:'desc 5',
+      img:img5,
+      
+    },
+    {
+        id:'6',
+        name:'img6',
+        title:'blog 6',
+        desc:'desc 6',
+        img:img6,
+    },
+    {
+        id:'7',
+        name:'img7',
+        title:'blog 7',
+        desc:'desc 7',
+        img:img3,
+        
+      },
+      {
+        id:'8',
+        name:'img8',
+        title:'blog 8',
+        desc:'desc 8',
+        img:img5,
+
+      },
+]
+}
+
+export default data;
